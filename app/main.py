1d1cc797f2782752007d444c9e5cf64033ed045a
leonardo <leodrl9@gmail.com>
2021-07-20 17:54:05 -0300

    strange behaviour

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
+import requests
 from app.src.sth_func import asd
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.post("/")
 async def root():
-    return {"message": "Hello World"}
+    return []
+
+@app.get("/up")
+async def root():
+    response = requests.post("http://localhost:8000/")
+    
+    return response
